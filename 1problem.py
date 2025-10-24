f56d289404a6010a18488291b98ecc06874554f4
asaloyomonboyeva23-rgb <asaloyomonboyeva23@gmail.com>
2025-10-24 09:06:54 +0500

    week4 problem1

--- 1problem.py
+++ 1problem.py
@@ -14,8 +14,6 @@ def  is_eligible_for_free_shipping(subtotal):
 def  process_order(item_name, quantity, unit_price, tax_rate):
      print (f"Order Receipt for:{item_name}")
      print (f"Quantity: {quantity}@ ${round(unit_price, 2)} each")
-
-
      item_total = calculate_item_total(quantity, unit_price)
      print (f" Item Total: ${item_total}")
      bulk_discount = apply_bulk_discount(item_total, quantity)
@@ -32,7 +30,7 @@ def  process_order(item_name, quantity, unit_price, tax_rate):
           need_for_free_shipping = 50- subtotal
           print(f"need {need_for_free_shipping :.2f} more for free shipping")
 print(process_order("notebooks", 12, 3.5 ,8))
-s
+
 
 
    
